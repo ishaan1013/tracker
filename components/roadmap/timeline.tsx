1d9575d1b96194f4ff438790e017a21eb09864b7
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-12-05 13:30:52 -0500

    collapsible roadmap left col

--- components/roadmap/timeline.tsx
+++ components/roadmap/timeline.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 import { PrismaClient, Issue } from "@prisma/client"
 import { prisma } from "../../prisma/db"
 import { IssueType } from "../../prisma/issueType"
 
-import { FiLoader, FiPlus, FiZap } from "react-icons/fi"
+import { FiChevronLeft, FiLoader, FiPlus, FiZap } from "react-icons/fi"
 
 const Timeline = () => {
   const monthRef = useRef<HTMLDivElement>(null)
+  const [collapsed, setCollapsed] = useState(false)
 
   useEffect(() => {
     const monthTo = monthRef.current
@@ -17,25 +18,37 @@ const Timeline = () => {
 
   return (
     <div className="mt-8 flex h-full w-full items-center rounded border-[1px] border-gray-300">
-      <div className="h-full w-60 min-w-[240px] rounded-l border-r-2 border-gray-300">
+      <div
+        className={`relative h-full rounded-l border-r-2 border-gray-300 ${
+          collapsed ? "w-20" : "w-48 min-w-[192px] lg:w-60 lg:min-w-[240px]"
+        }`}>
+        <button
+          onClick={() => setCollapsed((prev) => !prev)}
+          className="group absolute top-1/2 -right-3 z-20 flex -translate-y-1/2 items-center justify-center rounded-full border-[1px] border-blue-700 bg-white p-0.5 backdrop-blur-md focus:outline-blue-500">
+          <FiChevronLeft
+            className={`h-4 w-4 rounded-full p-[0.5px] text-blue-700 group-hover:bg-blue-200 ${
+              collapsed ? "translate-x-[1px] rotate-180" : ""
+            }`}
+          />
+        </button>
         <div className="flex h-14 w-full items-center justify-center rounded-tl border-b-[1px] border-gray-300 bg-gray-150 text-gray-600">
-          Projects
+          {collapsed ? null : "Projects"}
         </div>
         <div className="mt-2 flex flex-col items-center justify-start px-2">
           <button
             className={`mb-3 flex w-full select-none items-center justify-center whitespace-nowrap rounded border-[1px] border-transparent bg-blue-700 py-1.5 pr-1 text-base text-white duration-100 hover:bg-blue-600 focus:outline-blue-500`}>
-            <FiZap className="mr-1" />
-            Project 1
+            <FiZap className={collapsed ? "" : "mr-1"} />
+            {collapsed ? null : "Project 1"}
           </button>
           <button
             className={`mb-3 flex w-full select-none items-center justify-center whitespace-nowrap rounded border-[1px] border-transparent bg-blue-700 py-1.5 pr-1 text-base text-white duration-100 hover:bg-blue-600 focus:outline-blue-500`}>
-            <FiLoader className="mr-1" />
-            Project 2
+            <FiLoader className={collapsed ? "" : "mr-1"} />
+            {collapsed ? null : "Project 2"}
           </button>
           <button
             className={`flex w-full select-none items-center justify-center whitespace-nowrap rounded border-[1px] border-gray-300 py-1.5 pr-1 text-base text-gray-600 duration-100 hover:bg-gray-150 focus:outline-blue-500`}>
-            <FiPlus className="mr-1" />
-            Create Epic
+            <FiPlus className={collapsed ? "" : "mr-1"} />
+            {collapsed ? null : "Create Epic"}
           </button>
         </div>
       </div>
