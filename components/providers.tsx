88970f1454370fb478b756ca10ef42be35f63d2d
ishaan1013 <ishaan1013@gmail.com>
2023-02-12 12:17:39 -0500

    ga

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,9 +1,17 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <>
+        <GoogleAnalytics trackPageViews />
+        {children}
+      </>
+    </SessionProvider>
+  )
 }
 
 export default Providers
